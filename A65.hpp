efb44665a8057a92351ce5d1835e8190379c6138
kand-chen <ilovegreen109@gmail.com>
2022-10-07 16:00:17 -0700

    lab6-5

--- A65.hpp
+++ A65.hpp
@@ -7,5 +7,23 @@ using namespace std;
 // complete two functions
 // swap() ; to swap all three values like (a, b, c) to (c, a, b)
 //              This function will use swaptwo().
+void swap(int a, int b, int c);
+void swaptwo(int a, int b);
+
+void getinput(int a, int b, int c){
+    cout << "Enter three integers, separated by spaces: \n";
+    cin >> a >> b >> c;
+    swap(a, b, c);
+}
+
+void swap(int a, int b, int c){
+    int numA;
+    cout << c << ", ";
+    swaptwo(a, b);
+}
+
+void swaptwo(int a, int b){
+    cout << a << ", " << b << endl;
+}
 
 // swaptwo() ; to swap two values (a, b) to (b, a)
